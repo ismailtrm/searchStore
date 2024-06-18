dc5d4cb003acb4d618e9227f83c886c36f0f9595
ismailtrm <ismailtarim7@gmail.com>
2024-06-18 16:23:07 +0300

    update

--- api/proxy.js
+++ api/proxy.js
@@ -3,9 +3,13 @@ const fetch = require('node-fetch');
 module.exports = async (req, res) => {
   const url = req.query.url;
   
-  const response = await fetch(url);
-  const data = await response.text();
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).send(data);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send('Error fetching data');
+  }
 };
--- script.js
+++ script.js
@@ -2,30 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchButton = document.getElementById('search-button');
     const searchQuery = document.getElementById('search-query');
     const resultsSection = document.getElementById('results');
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const proxyUrl = 'https://search-store.vercel.app/api/proxy?url=';
     
     searchButton.addEventListener('click', function() {
         const query = searchQuery.value;
         if (query.trim() !== "") {
             const url = `https://www.trendyol.com/sr?q=${query}`;
-            fetch(`${url}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-      })
-      .then(data => {
-        // İçeriği seçilen bölüme ekleme
-        const contentSection = document.getElementById('content-section');
-        contentSection.innerHTML = `
-          <h2>${data.title}</h2>
-          <p>${data.body}</p>
-        `;
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
+            fetch(proxyUrl + encodeURIComponent(url))
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+              }
+              return response.text();
+            })
+            .then(html => {
+              const contentSection = document.getElementById('content-section');
+              contentSection.innerHTML = html;
+            })
+            .catch(error => {
+              console.error('There was a problem with the fetch operation:', error);
+            });
             resultsSection.innerHTML = `
                 <div>
                     <h2>Trendyol</h2>
