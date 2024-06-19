faf1f45ef4e337ca5dfe9f988b9b3f90d1f4efe2
İsmail Tarım <68031925+ismailtrm@users.noreply.github.com>
2024-06-19 16:10:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function() {
             resultsSection.innerHTML = `
                 <div>
                     <h2>Trendyol</h2>
-                    <p><a href="${url}" target="_blank">Arama Sonuçları</a></p>
+                    <p><a href="${url}" target="_blank">Arama Sonuçları Yükleniyor...</a></p>
                 </div>
             `;
         } else {
